a0e9aedeb9efd5e5b2060a8c4fa452cb0ff8eab2
Henrique Toledo <henriq_toledo@yahoo.com.br>
2020-06-04 18:25:22 -0300

    Add the entities in the context.

--- src/Questioner/Questioner.Repository/Classes/Entities/Context.cs
+++ src/Questioner/Questioner.Repository/Classes/Entities/Context.cs
@@ -5,6 +5,12 @@ namespace Questioner.Repository.Classes.Entities
 {
     public class Context : DbContext, IContext
     {
+        public DbSet<Theme> Themes { get; set; }
+        public DbSet<Topic> Topics { get; set; }
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<Answer> Answers { get; set; }
+        public DbSet<Link> Links { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             // Configures the database connection string
--- src/Questioner/Questioner.Repository/Interfaces/IContext.cs
+++ src/Questioner/Questioner.Repository/Interfaces/IContext.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Questioner.Repository.Classes.Entities;
+
 namespace Questioner.Repository.Interfaces
 {
     public interface IContext
     {
-         
+        DbSet<Theme> Themes { get; set; }
+        DbSet<Topic> Topics { get; set; }
+        DbSet<Question> Questions { get; set; }
+        DbSet<Answer> Answers { get; set; }
+        DbSet<Link> Links { get; set; }
     }
 }
