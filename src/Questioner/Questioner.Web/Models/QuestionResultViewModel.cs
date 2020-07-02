e0d19a0722c8beaee6337900d2369d24f854fee1
Henrique Toledo <henriq_toledo@yahoo.com.br>
2020-07-02 20:54:21 -0300

    Add the calc of percentage of the answered questions in the result view.

--- src/Questioner/Questioner.Web/Models/QuestionResultViewModel.cs
+++ src/Questioner/Questioner.Web/Models/QuestionResultViewModel.cs
@@ -7,10 +7,12 @@ namespace Questioner.Web.Models
     public class QuestionResultViewModel
     {
         public long Id { get; set; }
-        
+
+        public long TopicId { get; set; }
+
         [DisplayName("Question")]
         public string QuestionText { get; set; }
-        
+
         [DisplayName("Correct")]
         public QuestionResultEnum QuestionResult { get; set; }
 
