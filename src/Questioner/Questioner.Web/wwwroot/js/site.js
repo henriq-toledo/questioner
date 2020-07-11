d1c1839155ce8ac2b30503f93129359883ec17eb
Henrique Toledo <henriq_toledo@yahoo.com.br>
2020-07-11 12:59:23 -0300

    Disable the answers when select all the possible answers in the questioner view.

--- src/Questioner/Questioner.Web/wwwroot/js/site.js
+++ src/Questioner/Questioner.Web/wwwroot/js/site.js
@@ -2,3 +2,44 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(document).ready(function () {
+    $(".check-box").each(function () {
+        $(this).unbind("click").bind("click", function (event) {          
+
+            var answers = event.target.parentNode.parentNode.getElementsByClassName("check-box");
+            var answersChecked = 0;            
+
+            for (var i = 0; i < answers.length; i++) {
+                var answer = answers[i];
+
+                if (answer.checked) {
+                    answersChecked++;
+                }
+            };
+
+            var howManyChoices = event.target.parentNode.parentNode.parentNode.children[0].children[2].getAttribute("value");
+
+            var disableAnswer = howManyChoices == answersChecked;
+
+            if (disableAnswer) {
+                for (var i = 0; i < answers.length; i++) {
+                    var answer = answers[i];
+
+                    if (answer.checked == false) {
+                        answer.disabled = true;
+                    }
+                };
+            }
+            else {
+                for (var i = 0; i < answers.length; i++) {
+                    var answer = answers[i];
+
+                    if (answer.disabled == true) {
+                        answer.disabled = false;
+                    }
+                };
+            }
+        });
+    });
+});
