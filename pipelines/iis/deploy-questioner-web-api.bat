86cf9ee3c71d6eb9feb577c4040e305fabfdea3b
Henrique Toledo <henriq_toledo@yahoo.com.br>
2020-07-18 14:06:11 -0300

    Create the pipelines folder to store IIS and Azure DevOps pipelines.

---
+++ pipelines/iis/deploy-questioner-web-api.bat
@@ -0,0 +1,23 @@
+REM Clean
+rmdir "C:\Temp\questioner-api\" /S /Q
+
+REM Download repository
+git clone https://github.com/henriq-toledo/questioner.git "C:\Temp\questioner-api\build"
+
+REM Publish
+dotnet publish --configuration Release --runtime win10-x64 --no-self-contained --output "C:\Temp\questioner-api\publish" "C:\Temp\questioner-api\build\src\Questioner\Questioner.WebApi\Questioner.WebApi.csproj"
+
+REM Stop apppool
+%systemroot%\system32\inetsrv\appcmd stop apppool /apppool.name:DefaultAppPool
+
+del "C:\inetpub\wwwroot\questioner-api\**" /S /Q
+
+REM Copy publish to web app folder
+xcopy "C:\Temp\questioner-api\publish\**" "C:\inetpub\wwwroot\questioner-api" /E /Q
+
+REM Start apppool
+%systemroot%\system32\inetsrv\appcmd start apppool /apppool.name:DefaultAppPool
+
+start firefox "http://localhost/questioner-api/"
+
+pause
---
+++ pipelines/iis/deploy-questioner-web.bat
@@ -0,0 +1,23 @@
+REM Clean
+rmdir "C:\Temp\questioner\" /S /Q
+
+REM Download repository
+git clone https://github.com/henriq-toledo/questioner.git "C:\Temp\questioner\build"
+
+REM Publish
+dotnet publish --configuration Release --runtime win10-x64 --no-self-contained --output "C:\Temp\questioner\publish" "C:\Temp\questioner\build\src\Questioner\Questioner.Web\Questioner.Web.csproj"
+
+REM Stop apppool
+%systemroot%\system32\inetsrv\appcmd stop apppool /apppool.name:DefaultAppPool
+
+del "C:\inetpub\wwwroot\questioner\**" /S /Q
+
+REM Copy publish to web app folder
+xcopy "C:\Temp\questioner\publish\**" "C:\inetpub\wwwroot\questioner" /E /Q
+
+REM Start apppool
+%systemroot%\system32\inetsrv\appcmd start apppool /apppool.name:DefaultAppPool
+
+start firefox "http://localhost/questioner/"
+
+pause
