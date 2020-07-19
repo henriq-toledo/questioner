ec2eaeaebe7044925d942425cc19e1049c7d70f5
Henrique Toledo <henriq_toledo@yahoo.com.br>
2020-07-19 12:59:57 -0300

    Add the get returning the id and name from themes and the delete where deletes a specific theme in the Web Api.

--- src/Questioner/Questioner.WebApi/Controllers/ThemeController.cs
+++ src/Questioner/Questioner.WebApi/Controllers/ThemeController.cs
@@ -1,6 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using Questioner.Repository.Classes.Entities;
 using Questioner.WebApi.Models;
 using Questioner.WebApi.Validators;
@@ -39,6 +43,38 @@ namespace Questioner.WebApi.Controllers
             else
             {
                 return new BadRequestObjectResult(errors);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult<Array> Get()
+        {
+            var themes = _context.Themes.Select(theme => new { Id = theme.Id, Name = theme.Name}).ToList();
+            var context = JsonConvert.SerializeObject(themes);
+
+            return themes.ToArray();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(long id)
+        {
+            var theme = _context.Themes.Find(id);
+
+            if (theme == null)
+            {
+                return NotFound();
+            }
+
+            try
+            {
+                _context.Themes.Remove(theme);
+                _context.SaveChanges();
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
             }
         }
     }
