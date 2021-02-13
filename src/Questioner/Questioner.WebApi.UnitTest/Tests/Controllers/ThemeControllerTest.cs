456cc3e662bff796ede1395b8a2e5fdf448cd1ac
Henrique Toledo <henriq_toledo@yahoo.com.br>
2021-02-13 13:31:44 -0300

    Add delete theme test.

--- src/Questioner/Questioner.WebApi.UnitTest/Tests/Controllers/ThemeControllerTest.cs
+++ src/Questioner/Questioner.WebApi.UnitTest/Tests/Controllers/ThemeControllerTest.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
 using Questioner.WebApi.Controllers;
 using Questioner.WebApi.Models;
 using Questioner.WebApi.Repositories;
@@ -52,6 +53,27 @@ namespace Questioner.WebApi.UnitTest.Tests.Controllers
             var actualTheme = context.Themes.FirstOrDefault();
 
             ThemeAssert.Assert(expectedTheme: ThemeDefault.ThemeWithChildren, actualTheme);
+        }
+
+        [Test]
+        public async Task ShouldDeleteTheme()
+        {
+            // Arrange
+            using var context = ContextFactory.CreateContext();
+            var themeRepository = new ThemeRepository(context);
+            var themeService = new ThemeService(themeRepository);
+            var themeController = new ThemeController(themeService);
+
+            context.Themes.Add(ThemeDefault.ThemeWithChildren);
+            await context.SaveChangesAsync();
+
+            var themeId = (await context.Themes.FirstOrDefaultAsync()).Id;
+
+            // Act
+            await themeController.Delete(themeId);
+
+            // Assert
+            Assert.False(context.Themes.Any(t => t.Id == themeId), $"The theme id {themeId} should be deleted.");
         }
     }
 }
