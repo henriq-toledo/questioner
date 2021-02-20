126ba1b91dea04fdd76d479fa9f30849c45dce42
Henrique Toledo <henriq_toledo@yahoo.com.br>
2021-02-20 18:57:31 -0300

    Add unit test to check when theme name is duplicated should be invalid.

--- src/Questioner/Questioner.WebApi.UnitTest/Tests/Validators/ThemeModelValidatorTest.cs
+++ src/Questioner/Questioner.WebApi.UnitTest/Tests/Validators/ThemeModelValidatorTest.cs
@@ -2,6 +2,7 @@
 using NUnit.Framework;
 using Questioner.WebApi.Models;
 using Questioner.WebApi.UnitTest.Framework.Constants;
+using Questioner.WebApi.UnitTest.Framework.Extensions;
 using Questioner.WebApi.UnitTest.Framework.Factories;
 using Questioner.WebApi.Validators;
 using System.Threading.Tasks;
@@ -39,6 +40,28 @@ namespace Questioner.WebApi.UnitTest.Tests.Validators
             result
                 .ShouldHaveValidationErrorFor(theme => theme.Name)
                 .WithErrorCode(FluentValidationErrorCodeConstant.NotEmptyValidator);
+        }
+
+        [Test]
+        public async Task DuplicatedThemeNameShouldBeInvalid()
+        {
+            // Arrange
+            var themeName = "Test";
+            var expectedErrorMessage = $"The '{themeName}' theme already exists.";
+            var theme = new Repository.Classes.Entities.Theme { Name = themeName };
+            var themeModel = new ThemeModel { Name = themeName };
+            var context = ContextFactory.Create();            
+            var themeModelValidator = new ThemeModelValidator(context);
+
+            await context.InsertTheme(theme);
+
+            // Act
+            var result = await themeModelValidator.TestValidateAsync(themeModel);
+
+            // Assert
+            result
+                .ShouldHaveValidationErrorFor(theme => theme.Name)
+                .WithErrorMessage(expectedErrorMessage);
         }
     }
 }
