d9dcc62a6a984c989805e064b3dd4aca697516bf
Henrique Toledo <henriq_toledo@yahoo.com.br>
2021-03-13 17:38:56 -0300

    Add unit test to the index method from home controller.

---
+++ src/Questioner/Questioner.Web.UnitTest/Framework/Asserts/ThemeListViewModelAssert.cs
@@ -0,0 +1,35 @@
+﻿using Questioner.Web.Models;
+using System.Collections.Generic;
+using System.Linq;
+using static NUnit.Framework.Assert;
+
+namespace Questioner.Web.UnitTest.Framework.Asserts
+{
+    public static class ThemeListViewModelAssert
+    {
+        public static void Assert(List<ThemeListViewModel> expectedThemeListViewModel, List<ThemeListViewModel> actualThemeListViewModel)
+        {
+            AreEqual(expectedThemeListViewModel?.Count, actualThemeListViewModel?.Count,
+                message: $"The expected number of themes should be {expectedThemeListViewModel?.Count} and not {actualThemeListViewModel?.Count}.");
+
+            foreach (var expectedTheme in expectedThemeListViewModel)
+            {
+                var actualTheme = actualThemeListViewModel.FirstOrDefault(t => t.Id == expectedTheme.Id);
+
+                NotNull(actualTheme, message: $"The theme '{expectedTheme.Name}' should exist.");
+
+                AreEqual(expectedTheme.Name, actualTheme.Name,
+                    message: $"For the theme id '{expectedTheme.Id}', the {nameof(expectedTheme.Name)} should be {expectedTheme.Name} and not {actualTheme.Name}.");
+
+                AreEqual(expectedTheme.PassRate, actualTheme.PassRate,
+                    message: $"For the theme id '{expectedTheme.Id}', the {nameof(expectedTheme.PassRate)} should be {expectedTheme.PassRate} and not {actualTheme.PassRate}.");
+
+                AreEqual(expectedTheme.TopicsQuantity, actualTheme.TopicsQuantity,
+                    message: $"For the theme id '{expectedTheme.Id}', the {nameof(expectedTheme.TopicsQuantity)} should be {expectedTheme.TopicsQuantity} and not {actualTheme.TopicsQuantity}.");
+
+                AreEqual(expectedTheme.QuestionsQuantity, actualTheme.QuestionsQuantity,
+                    message: $"For the theme id '{expectedTheme.Id}', the {nameof(expectedTheme.QuestionsQuantity)} should be {expectedTheme.QuestionsQuantity} and not {actualTheme.QuestionsQuantity}.");
+            }
+        }
+    }
+}
---
+++ src/Questioner/Questioner.Web.UnitTest/Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using Questioner.Repository.Classes.Entities;
+using Questioner.Web.Controllers;
+using Questioner.Web.Models;
+using Questioner.Web.Services;
+using Questioner.Web.UnitTest.Framework.Asserts;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Questioner.Web.UnitTest.Tests.Controllers
+{
+    [TestFixture]
+    public class HomeControllerTest
+    {
+        [Test]
+        public async Task IndexShouldReturnAllThemes()
+        {
+            Theme[] themes = new Theme[]
+            {
+                new Theme
+                {
+                    Id = 1,
+                    Name = "Test 1",
+                    PassRate = 60,
+                    Topics = new List<Topic>
+                    {
+                        new Topic
+                        {
+                            Questions = new List<Question>
+                            {
+                                new Question(),
+                                new Question()
+                            }
+                        },
+                        new Topic
+                        {
+                            Questions = new List<Question>
+                            {
+                                new Question(),
+                                new Question(),
+                                new Question()
+                            }
+                        },
+                    }
+                }
+            };
+
+            // Arrange
+            var themeServiceMock = new Mock<IThemeService>();
+            themeServiceMock.Setup(t => t.GetAllThemes()).ReturnsAsync(themes);
+
+            var homeController = new HomeController(themeServiceMock.Object);
+
+            // Act
+            var result = (await homeController.Index()) as ViewResult;
+
+            // Assert
+            Assert.IsInstanceOf<List<ThemeListViewModel>>(result.Model, message: "");
+
+            var actualThemeListViewModel = result.Model as List<ThemeListViewModel>;
+
+            ThemeListViewModelAssert.Assert(expectedThemeListViewModel: themes.Select(theme => new ThemeListViewModel(theme)).ToList(), actualThemeListViewModel);
+        }
+    }
+}
