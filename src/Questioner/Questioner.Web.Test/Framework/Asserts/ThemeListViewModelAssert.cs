410c9b593e83b4e81c657372d889b51ca835754d
Henrique Toledo <henriq_toledo@yahoo.com.br>
2023-03-04 20:26:29 -0300

    Add unit test to the Index method of the HomeController class.

---
+++ src/Questioner/Questioner.Web.Test/Framework/Asserts/ThemeListViewModelAssert.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using Questioner.Web.Models;
+using System.Collections.Generic;
+
+namespace Questioner.Web.Test.Framework.Asserts
+{
+    internal static class ThemeListViewModelAssert
+    {
+        public static void AreEqual(List<ThemeListViewModel> expected, List<ThemeListViewModel> actual)
+        {
+            Assert.AreEqual(expected.Count, actual.Count);
+
+            for (int i = 0; i < expected.Count; i++)
+            {
+                var expectedThemeListViewModel = expected[i];
+                var actualThemeListViewModel = actual[i];
+
+                Assert.AreEqual(expectedThemeListViewModel.Id, actualThemeListViewModel.Id);
+                Assert.AreEqual(expectedThemeListViewModel.Name, actualThemeListViewModel.Name);
+                Assert.AreEqual(expectedThemeListViewModel.PassRate, actualThemeListViewModel.PassRate);
+                Assert.AreEqual(expectedThemeListViewModel.TopicsQuantity, actualThemeListViewModel.TopicsQuantity);
+                Assert.AreEqual(expectedThemeListViewModel.QuestionsQuantity, actualThemeListViewModel.QuestionsQuantity);
+            }
+        }
+    }
+}
---
+++ src/Questioner/Questioner.Web.Test/Tests/HomeControllerTest.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using Questioner.Repository.Classes.Entities;
+using Questioner.Web.Controllers;
+using Questioner.Web.Models;
+using Questioner.Web.Services;
+using Questioner.Web.Test.Framework.Asserts;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Questioner.Web.Test.Tests
+{
+    internal class HomeControllerTest
+    {
+        private HomeController homeController;
+        private Mock<IThemeService> themeServiceMock;
+
+        [SetUp]
+        public void SetUp()
+        {
+            themeServiceMock = new Mock<IThemeService>();
+
+            homeController = new HomeController(themeServiceMock.Object);
+        }
+
+        [Test]
+        public async Task Index_WhenCalled_ReturnsThemes()
+        {
+            // Arrange
+            const int themeId = 1;
+            const byte passRate = 80;
+            const string themeName = "Test";
+            var themes = new Theme[]
+            {
+                new Theme
+                {
+                    Id = themeId,
+                    Name = themeName,
+                    PassRate = passRate,
+                    Topics = new List<Topic>
+                    {
+                        new Topic 
+                        {
+                            Questions = new List<Question>
+                            { 
+                                new Question(),
+                                new Question()
+                            }
+                        }
+                    }
+                }
+            };
+            var expectedThemeListViewModels = new List<ThemeListViewModel>
+            { 
+                new ThemeListViewModel
+                {
+                    Id = themeId,
+                    Name = themeName,
+                    PassRate = passRate,
+                    TopicsQuantity = 1,
+                    QuestionsQuantity = 2
+                }
+            };
+            themeServiceMock.Setup(m => m.GetAllThemes()).ReturnsAsync(themes);
+
+            // Act
+            var actionResult = (ViewResult)await homeController.Index();
+
+            // Assert
+            Assert.IsInstanceOf<List<ThemeListViewModel>>(actionResult.Model);
+            ThemeListViewModelAssert.AreEqual(expectedThemeListViewModels, (List<ThemeListViewModel>)actionResult.Model);
+        }
+    }
+}
