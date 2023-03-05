96f3ef511ebc8bb0e113b448d52ee9bf63529e59
Henrique Toledo <henriq_toledo@yahoo.com.br>
2023-03-05 11:22:43 -0300

    Add unit test to the Details method of the ThemeController class.

---
+++ src/Questioner/Questioner.Web.Test/Tests/ThemeControllerTest.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using Questioner.Web.Controllers;
+using Questioner.Web.Services;
+using System.Threading.Tasks;
+
+namespace Questioner.Web.Test.Tests
+{
+    internal class ThemeControllerTest
+    {
+        private ThemeController themeController;
+        private Mock<IThemeService> themeServiceMock;
+
+        [SetUp]
+        public void SetUp()
+        {
+            themeServiceMock = new Mock<IThemeService>();
+
+            themeController = new ThemeController(themeServiceMock.Object);
+        }
+
+        [Test]
+        public async Task Details_WithInexistentThemeId_ReturnsNotFound()
+        {
+            // Arrange
+            const int themeId = 1;
+
+            // Act
+            var actionResult = await themeController.Details(themeId);
+
+            // Assert
+            Assert.IsInstanceOf<NotFoundResult>(actionResult);
+            themeServiceMock.Verify(m => m.GetThemeById(themeId), Times.Once);
+        }
+    }
+}
