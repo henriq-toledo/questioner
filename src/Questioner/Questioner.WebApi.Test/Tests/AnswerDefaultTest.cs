cce4eff57175d2b4dcb585d614bf6a258a996ec7
Henrique Toledo <henriq_toledo@yahoo.com.br>
2023-03-21 19:39:55 -0300

    Add unit test to the AnswerDefault class.

---
+++ src/Questioner/Questioner.WebApi.Test/Tests/AnswerDefaultTest.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using Questioner.WebApi.Defaults;
+
+namespace Questioner.WebApi.Test.Tests
+{
+    internal class AnswerDefaultTest
+    {
+        [Test]
+        public void NoYes_WhenCalled_Retuns()
+        {
+            // Arrange
+
+            // Act
+            var noYes = AnswerDefault.NoYes;
+
+            // Assert
+            Assert.That(noYes.Length, Is.EqualTo(2));
+
+            var no = noYes[0];
+
+            Assert.That(no.IsCorrect, Is.False);
+            Assert.That(no.AnswerText, Is.EqualTo("False"));
+
+            var yes = noYes[1];
+
+            Assert.That(yes.IsCorrect, Is.True);
+            Assert.That(yes.AnswerText, Is.EqualTo("True"));
+        }
+    }
+}
