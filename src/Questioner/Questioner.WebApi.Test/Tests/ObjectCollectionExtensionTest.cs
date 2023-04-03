53a66f0543b7ee71b34cd08859309f3d936c589e
Henrique Toledo <henriq_toledo@yahoo.com.br>
2023-04-03 18:40:27 -0300

    Add unit test to the ObjectCollectionExtension class.

---
+++ src/Questioner/Questioner.WebApi.Test/Tests/ObjectCollectionExtensionTest.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using Questioner.WebApi.Extensions;
+
+namespace Questioner.WebApi.Test.Tests
+{
+    internal class ObjectCollectionExtensionTest
+    {
+        [Test]
+        [TestCase(null, true)]
+        [TestCase(new int[] { }, true)]
+        [TestCase(new int[] { 0 }, false)]
+        public void IsNullOrEmpty_WhenCalled_Returns(int[] collection, bool expectedResult)
+        {
+            // Act
+            var actualResult = collection.IsNullOrEmpty();
+
+            // Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
