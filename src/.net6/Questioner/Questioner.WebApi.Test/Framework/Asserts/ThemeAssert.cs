8ebe7b0c582591fe9a83a5ce7503ebdccf5aa57a
Henrique Toledo <henriq_toledo@yahoo.com.br>
2023-04-12 19:14:01 -0300

    Using the constraint model in the asserts.

--- src/.net6/Questioner/Questioner.WebApi.Test/Framework/Asserts/ThemeAssert.cs
+++ src/.net6/Questioner/Questioner.WebApi.Test/Framework/Asserts/ThemeAssert.cs
@@ -10,16 +10,16 @@ namespace Questioner.WebApi.Test.Framework.Asserts
 
         public static void Assert(Theme[] expectedThemes, Theme[] actualThemes)
         {
-            AreEqual(expectedThemes?.Length, actualThemes?.Length, 
+            That(actualThemes?.Length, Is.EqualTo(expectedThemes?.Length), 
                 message: $"The expected number of themes should be {expectedThemes?.Length} and not {actualThemes?.Length}.");
 
             foreach (var expectedTheme in expectedThemes)
             {
                 var actualTheme = actualThemes.FirstOrDefault(t => t.Name == expectedTheme.Name);
 
-                NotNull(actualTheme, message: $"The theme '{expectedTheme.Name}' should exist.");
+                That(actualTheme, Is.Not.Null, message: $"The theme '{expectedTheme.Name}' should exist.");
 
-                AreEqual(expectedTheme.PassRate, actualTheme.PassRate,
+                That(actualTheme.PassRate, Is.EqualTo(expectedTheme.PassRate),
                     message: $"For the theme '{expectedTheme.Name}', the {nameof(expectedTheme.PassRate)} should be {expectedTheme.PassRate} and not {actualTheme.PassRate}.");
 
                 TopicAssert.Assert(expectedTopics: expectedTheme?.Topics, actualTopics: actualTheme?.Topics);
